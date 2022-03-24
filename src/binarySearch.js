348df4d2ab754c3878f530266672cc6d52af8cca
DESKTOP-UAFR4CS\scra <scra1028@gmail.com>
2022-03-24 21:30:11 +0900

    feat: 객체에 대한 이진검색 구현
    - 매개변수 comparator의 유무에 따라 서로 다른 방식으로 이진검색을 구현함
    - 일단 테스트코드는 통과하는데, 중복코드가 존재함. 이를 해소할 수 있는 방법이 있는지 찾아보는게 좋겠음

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -1,4 +1,5 @@
 //값을 찾지 못한 경우에 해당하는 상수값.
+
 const NOT_FOUND = -1;
 
 /**
@@ -7,10 +8,22 @@ const NOT_FOUND = -1;
  * 이진 검색은 대상 값을 배열의 중간 요소와 비교합니다.
  * 그들이 같지 않으면 목표물이 결과가 거짓인 절반은 제거되고 성공할 때까지 나머지 절반에서 검색이 계속됩니다.
  * 나머지 절반이 비어 있는 상태에서 검색이 끝나면 대상이 배열에 없는 것입니다.
+ * 해당 함수는 숫자에 대한 이진검색을 수행합니다
+ * 비교함수인 comparator가 있으면 객체 이진검색을, 없으면 숫자 이진검색을 수행합니다.
  * @param array 이진 검색을 수행할 배열
  * @param target 위치를 찾고자 하는 대상 값
+ * @param comparator 객체 이진 검색에 쓰이는 비교함수
  */
-export default function binarySearch(array, target) {
+export default function binarySearch(array, target, comparator) {
+    //comparator가 있으면 객체 이진검색, 없으면 숫자 이진검색을 수행
+    if (comparator) {
+        return objectBinarySearch(array, target, comparator);
+    } else {
+        return numberBinarySearch(array, target);
+    }
+}
+
+function numberBinarySearch(array, target) {
     //low는 최소값의 인덱스, high는 최대값의 인덱스.
     let low = 0;
     let high = array.length - 1;
@@ -28,4 +41,24 @@ export default function binarySearch(array, target) {
         }
     }
     return NOT_FOUND;//값을 찾지 못한 경우, 이에 해당하는 상수값을 반환.
-};
+}
+
+function objectBinarySearch(array, targetObject, comparator) {
+    //low는 최소값의 인덱스, high는 최대값의 인덱스.
+    let low = 0;
+    let high = array.length - 1;
+    //최소값의 인덱스가 최대값의 인덱스보다 작거나 같을때만 수행
+    while (low <= high) {
+        //배열의 중간에 해당하는 인덱스를 구하되, 몫만 구할 수 있도록 내림 함수 사용
+        const medium = Math.floor((low + high) / 2);
+        const mediumObject = array[medium];
+        if (comparator(mediumObject, targetObject) === 0) {//중간값이 대상과 같다면, 그 위치를 반환.
+            return medium;
+        } else if (comparator(mediumObject, targetObject) === 1) {//중간값이 대상보다 크다면, 최대값의 인덱스를 중간값의 이전 인덱스로 재설정.
+            high = medium - 1;
+        } else {//반대로 중간값이 작으면, 최소갑사의 인덱스를 중간값의 다음 값으로 재설정.
+            low = medium + 1;
+        }
+    }
+    return NOT_FOUND;//값을 찾지 못한 경우, 이에 해당하는 상수값을 반환.
+}
